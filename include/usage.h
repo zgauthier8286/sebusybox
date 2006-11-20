15e9bf4f6668f88aa1d26ff1cefc679e5b3021a8
himainu <himainu@2c370296-a921-0410-97cd-1f0e6b9e817e>
2006-11-20 08:31:51 +0000

    =Added chcon applet

--- include/usage.h
+++ include/usage.h
@@ -2376,6 +2376,30 @@ USE_FEATURE_MDEV_CONFIG( \
 #define runcon_example_usage \
 	"$ runcon -l s0 bash\n"
 
+#define chcon_trivial_usage \
+	"chcon [OPTIONS] CONTEXT FILE...\n"						\
+	"chcon [OPTIONS] [-u USER] [-r ROLE] [-l RANGE] [-t TYPE] FILE...\n"		\
+	"chcon [OPTIONS] --reference=RFILE FILE...\n"
+
+#define chcon_full_usage \
+	"Change the security context of each FILE to CONTEXT.\n\n"			\
+	"-c, --changes          like verbose but report only when a change is made\n"	\
+	"-h, --no-dereference   affect symbolic links instead of any referenced file\n"	\
+	"                       (available only on systems with lchown system call)\n"	\
+	"-f, --silent, --quiet  suppress most error messages\n"				\
+	"    --reference=RFILE  use RFILE's group instead of using a CONTEXT value\n"	\
+	"-u, --user=USER        set user USER in the target security context\n"		\
+	"-r, --role=ROLE        set role ROLE in the target security context\n"		\
+	"-t, --type=TYPE        set type TYPE in the target security context\n"		\
+	"-l, --range=RANGE      set range RANGE in the target security context\n"	\
+	"-R, --recursive        change files and directories recursively\n"		\
+	"-v, --verbose          output a diagnostic for every file processed\n"		\
+	"    --help             display this help and exit\n"				\
+	"    --version          output version information and exit\n"
+
+#define chcon_example_usage \
+	"$ chcon -t user_home_t memo.txt\n"
+
 #if ENABLE_FEATURE_PS_WIDE
 #define USAGE_PS_WIDE(a) a
 #else
